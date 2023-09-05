3a22d9c30c9718097f772c813cda991cee70660f
dodam24 <dodam0724@gmail.com>
2023-09-05 19:42:57 +0900

    [자바스크립트] Live Server와 DOMContentLoaded 이벤트

---
+++ DOM/DOMContentLoaded.js
@@ -0,0 +1,14 @@
+// DOMContentLoaded : 브라우저가 HTML을 전부 읽고 DOM 트리를 완성하는 즉시 발생
+// 즉, 문서의 로딩되는 시점 이벤트 제어하기
+
+
+// 문서 객체 모델 (DOM, Document Object Model)
+// 문서 객체: HTML 요소
+// 문서 객체 모델: 그것을 조작하는 객체들의 집합
+
+// DOMContentLoaded 이벤트
+
+
+// document.body.innerHTML += '<h1></h1>'
+//  : 문서 객체의 body 객체 아래에 있는 innerHTML에 += 을 활용해서 문자열을 출력하는 코드
+// innerHTML: 문자열 이므로 += 을 이용해 문자열을 추가할 수 있음
