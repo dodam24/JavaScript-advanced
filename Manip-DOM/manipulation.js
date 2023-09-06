a11edc0b5708031316628f3a665fdd0d981e173b
dodam24 <dodam0724@gmail.com>
2023-09-06 23:37:22 +0900

    [자바스크립트] 문서 객체 조작

---
+++ Manip-DOM/manipulation.js
@@ -0,0 +1,16 @@
+// 글자 조작
+// textContent: 모든 텍스트를 그대로 가져옴
+// innerHTML: 불필요한 공백을 제거하고 텍스트로 반환
+
+
+// 속성 조작
+setAttribute('이름', '값')
+getAttribute('이름')
+// 표준에 있는 속성이 아닐 경우, 그냥 입력
+// ex) port. img.src = 'http://github.com/img'
+
+
+// 스타일 조작
+style.backgrondColor
+style['background-color']
+// 두 가지 형태 모두 가능. 일반적으로 카멜 케이스 형태의 코드를 더 많이 사용함
